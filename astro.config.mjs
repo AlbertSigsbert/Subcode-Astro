06cdc48107958d18f84eda0640284b63e361c9e9
Albert <ishengomaalbert@gmail.com>
2023-02-06 07:00:27 +0300

    Minor fixes &changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://subcode-afrika.netlify.app",
+  site: "http://www.subcode.africa",
   integrations: [tailwind(), alpinejs(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
   }), robotsTxt()]
